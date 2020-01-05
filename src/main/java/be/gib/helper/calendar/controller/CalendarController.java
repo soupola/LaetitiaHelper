8b1a7cdd5590e96533a8352b379804e3746cdbac
gillesbeaucarne <gilles.beaucarne@gmail.com>
2020-01-05 15:33:48 +0100

    load all pie

--- src/main/java/be/gib/helper/calendar/controller/CalendarController.java
+++ src/main/java/be/gib/helper/calendar/controller/CalendarController.java
@@ -2,6 +2,7 @@ package be.gib.helper.calendar.controller;
 
 import be.gib.helper.calendar.builder.CalendarFactory;
 import be.gib.helper.calendar.builder.calendarfx.CalendarFactoryImpl;
+import be.gib.helper.core.bean.Scheduler;
 import be.gib.helper.core.controller.MainController;
 import be.gib.helper.stat.builder.ChartFactory;
 import be.gib.helper.stat.builder.pie.NbePieBuilder;
@@ -9,8 +10,11 @@ import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
+import sun.tools.jconsole.inspector.XNodeInfo;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class CalendarController extends MainController {
 
@@ -22,16 +26,20 @@ public class CalendarController extends MainController {
         assert mainCalendar != null : "fx:id=\"mainCalendar\" was not injected: check your FXML file 'calendarView.fxml'.";
         setCalendarController(this);
         if (!getSchedulers().isEmpty()) {
-           // CalendarFactory factory = new CalendarFactoryImpl();
-          //  Node calendar = factory.getLoadedCalendar(getSchedulers());
+            // CalendarFactory factory = new CalendarFactoryImpl();
+            //  Node calendar = factory.getLoadedCalendar(getSchedulers());
 
-            ArrayList<Node> graphs = new ArrayList();
-            graphs.add(ChartFactory.generateNbeChart(getSchedulers().get(0)));
+            //Load graphs
+            Map<String, Node> graphs = new HashMap<>();
+            for (Scheduler scheduler : getSchedulers()) {
+                graphs.put("Category for " + scheduler.getName(), ChartFactory.generateNbeChart(scheduler));
+            }
 
-            for (Node node : graphs) {
+            for (Map.Entry<String, Node> entry : graphs.entrySet()) {
                 Tab tab = new Tab();
+                tab.setText(entry.getKey());
                 tab.setClosable(false);
-                tab.setContent(node);
+                tab.setContent(entry.getValue());
                 mainCalendar.getTabs().add(tab);
             }
         } else {
